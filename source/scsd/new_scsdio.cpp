e104afdbf14c06d37f6ceb006e15a4de1a5c9115
ausar <464270342@qq.com>
2024-03-03 01:00:02 +0800

    fix warrning
    Signed-off-by: ausar <464270342@qq.com>

--- source/scsd/new_scsdio.cpp
+++ source/scsd/new_scsdio.cpp
@@ -40,7 +40,7 @@ void ReadSector(uint16_t *buff, uint32_t sector, uint32_t readnum)
 {
     auto param = isSDHC ? sector : (sector << 9);
     SDCommand(0x12,param); // R0 = 0x12, R1 = 0, R2 as calculated above
-    for(int i=0;i<readnum;i++)
+    for(u32 i=0;i<readnum;i++)
     {
         _SCSD_readData(buff); // Add R6, left shifted by 9, to R4 before casting
         buff += 512 / 2;
